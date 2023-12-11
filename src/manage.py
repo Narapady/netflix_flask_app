861eb6949e0b4608ab3002c4b2a638a86bb93804
Narapady <chhuoynarapady@yahoo.com>
2023-12-10 17:32:34 -0800

    added query parameter

--- src/manage.py
+++ src/manage.py
@@ -1,11 +1,18 @@
 import psycopg2
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 from src.models.credit import Credit
 from src.models.title import Title
 from src.settings import DB_NAME, PASSWORD, USER
 
 app = Flask(__name__)
+
+
+def build_query_args(model):
+    keys = model.columns
+    values = [request.args.get(col) for col in model.columns]
+    query_args = dict(zip(keys, values))
+    return {k: v for k, v in query_args.items() if v is not None}
 
 
 @app.route("/titles")
@@ -16,7 +23,23 @@ def titles():
         password=PASSWORD,
     )
     cursor = conn.cursor()
-    cursor.execute("select * from title;")
+    query_args = build_query_args(Title)
+
+    if query_args:
+        sql_str = "select * from title where "
+        for k, v in query_args.items():
+            sql_str += f"{k} = %s and "
+        sql_str = sql_str[:-5] + ";"
+
+        cursor.execute(
+            sql_str,
+            tuple(
+                query_args.values(),
+            ),
+        )
+    else:
+        cursor.execute("select * from title;")
+
     title_records = cursor.fetchall()
     return jsonify([Title(title_record).__dict__ for title_record in title_records])
 
